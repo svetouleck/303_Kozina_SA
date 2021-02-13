24ecb881c925f1ffd85d8f48596266d67dbac70c
Sveta Kozina <swet.kozina2001@yandex.ru>
2021-02-13 11:27:33 +0300

    Edit Task02

--- Task02/make_db_init.py
+++ Task02/make_db_init.py
@@ -41,31 +41,31 @@ def parse_line(s):
 
 
 with open("db_init.sql", "w", encoding="utf-8") as fout:
-    print(".open movies_rating.db", end="\n", file=fout)
+    print(".open movies_rating.db\n", file=fout)
     
     # удаление таблиц, если они уже существуют
-    print("DROP TABLE IF EXISTS movies;", end="\n", file=fout)
-    print("DROP TABLE IF EXISTS ratings;", end="\n", file=fout)
-    print("DROP TABLE IF EXISTS tags;", end="\n", file=fout)
-    print("DROP TABLE IF EXISTS users;", end="\n", file=fout)
+    print("DROP TABLE IF EXISTS movies;\n", file=fout)
+    print("DROP TABLE IF EXISTS ratings;\n", file=fout)
+    print("DROP TABLE IF EXISTS tags;\n", file=fout)
+    print("DROP TABLE IF EXISTS users;\n", file=fout)
     
     
     
     # создание таблиц
-    print("CREATE TABLE movies (id INTEGER RRIMARY KEY, title VARCHAR, year INTEGER, genres VARCHAR);",
-          end="\n", file=fout)
-    print("CREATE TABLE ratings (id INTEGER RRIMARY KEY, user_id INTEGER, movie_id INTEGER, rating FLOAT, timestamp INTEGER);",
-          end="\n", file=fout)
-    print("CREATE TABLE tags (id INTEGER RRIMARY KEY, user_id INTEGER, movie_id INTEGER, tag VARCHAR, timestamp INTEGER);",
-          end="\n", file=fout)
-    print("CREATE TABLE users (id INTEGER RRIMARY KEY, name VARCHAR, email VARCHAR, gender VARCHAR, register_date DATE, occupation VARCHAR);",
-          end="\n", file=fout)
+    print("CREATE TABLE movies (id INTEGER RRIMARY KEY, title VARCHAR, year INTEGER, genres VARCHAR);\n",
+          file=fout)
+    print("CREATE TABLE ratings (id INTEGER RRIMARY KEY, user_id INTEGER, movie_id INTEGER, rating FLOAT, timestamp INTEGER);\n", 
+          file=fout)
+    print("CREATE TABLE tags (id INTEGER RRIMARY KEY, user_id INTEGER, movie_id INTEGER, tag VARCHAR, timestamp INTEGER);\n", 
+          file=fout)
+    print("CREATE TABLE users (id INTEGER RRIMARY KEY, name VARCHAR, email VARCHAR, gender VARCHAR, register_date DATE, occupation VARCHAR);\n", 
+          file=fout)
     
     
     
     # парсинг файла movies.csv и заполнение таблицы movies
-    print("INSERT INTO movies (id, title, year, genres)", end="\n", file=fout)
-    print(f"VALUES", end=" ", file=fout)
+    print("INSERT INTO movies (id, title, year, genres)\n", file=fout)
+    print(f"VALUES ", file=fout)
 
     with open("movies.csv", "r", encoding="utf-8") as f:
         file = f.readlines()[1:]
@@ -74,14 +74,14 @@ with open("db_init.sql", "w", encoding="utf-8") as fout:
             #data.append(parse_line(row[:-1]))
             item = parse_line(file[i][:-1])
             if i != len(file)-1:
-                print(f"({item[0]}, '{item[3]}', {item[2]}, '{item[1]}')", end=",\n", file=fout)
+                print(f"({item[0]}, '{item[3]}', {item[2]}, '{item[1]}'),\n", file=fout)
             else:
-                print(f"({item[0]}, '{item[3]}', {item[2]}, '{item[1]}')", end=";\n", file=fout) 
+                print(f"({item[0]}, '{item[3]}', {item[2]}, '{item[1]}');\n", file=fout) 
                 
                 
                 
     # парсинг файла ratings.csv и заполнение таблицы ratings
-    print("INSERT INTO ratings (id, user_id, movie_id, rating, timestamp)", end="\n", file=fout)
+    print("INSERT INTO ratings (id, user_id, movie_id, rating, timestamp)\n", file=fout)
     print(f"VALUES", end=" ", file=fout)
     
     with open("ratings.csv", "r") as fl:
@@ -89,14 +89,14 @@ with open("db_init.sql", "w", encoding="utf-8") as fout:
         for i in range(len(file)):
             item = file[i][:-1].split(",")
             if i != len(file)-1:
-                print(f"({i}, {item[0]}, {item[1]}, {item[2]}, {item[3]})", end=",\n", file=fout)
+                print(f"({i}, {item[0]}, {item[1]}, {item[2]}, {item[3]}),\n", file=fout)
             else:
-                print(f"({i}, {item[0]}, {item[1]}, {item[2]}, {item[3]})", end=";\n", file=fout) 
+                print(f"({i}, {item[0]}, {item[1]}, {item[2]}, {item[3]});\n", file=fout) 
                 
       
         
     # парсинг файла tags.csv и заполнение таблицы tags           
-    print("INSERT INTO tags (id, user_id, movie_id, tag, timestamp)", end="\n", file=fout)
+    print("INSERT INTO tags (id, user_id, movie_id, tag, timestamp)\n", file=fout)
     print(f"VALUES", end=" ", file=fout)
     
     with open("tags.csv", "r") as fl:
@@ -106,14 +106,14 @@ with open("db_init.sql", "w", encoding="utf-8") as fout:
             item = item.replace("'", "‘")
             item = item.split(",")
             if i != len(file)-1:
-                print(f"({i}, {item[0]}, {item[1]}, '{item[2]}', {item[3]})", end=",\n", file=fout)
+                print(f"({i}, {item[0]}, {item[1]}, '{item[2]}', {item[3]}),\n", file=fout)
             else:
-                print(f"({i}, {item[0]}, {item[1]}, '{item[2]}', {item[3]})", end=";\n", file=fout) 
+                print(f"({i}, {item[0]}, {item[1]}, '{item[2]}', {item[3]});\n", file=fout) 
                 
             
             
     # парсинг файла users.txt и заполнение таблицы users            
-    print("INSERT INTO users (id, name, email, gender, register_date, occupation)", end="\n", file=fout)
+    print("INSERT INTO users (id, name, email, gender, register_date, occupation)\n", file=fout)
     print(f"VALUES", end=" ", file=fout)
     
     with open("users.txt", "r") as fl:
@@ -123,9 +123,9 @@ with open("db_init.sql", "w", encoding="utf-8") as fout:
             item = item.replace("'", "‘")
             item = item.split("|")
             if i != len(file)-1:
-                print(f"( {item[0]}, '{item[1]}', '{item[2]}', '{item[3]}', {item[4]}, '{item[5]}')", end=",\n", file=fout)
+                print(f"( {item[0]}, '{item[1]}', '{item[2]}', '{item[3]}', {item[4]}, '{item[5]}'),\n", file=fout)
             else:
-                print(f"( {item[0]}, '{item[1]}', '{item[2]}', '{item[3]}', {item[4]}, '{item[5]}')", end=";\n", file=fout) 
+                print(f"( {item[0]}, '{item[1]}', '{item[2]}', '{item[3]}', {item[4]}, '{item[5]}');\n", file=fout) 
                 
                 
                 
